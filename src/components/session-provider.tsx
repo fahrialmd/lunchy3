ec2af60446c917cf9d2863dd87b6557e0cf9440f
fahrialmd <a.fahrialhamdi@gmail.com>
2025-09-09 16:37:18 +0700

    fix: correct return statement formatting in AuthSessionProvider component

--- src/components/session-provider.tsx
+++ src/components/session-provider.tsx
@@ -7,12 +7,13 @@ export function AuthSessionProvider({
 }: {
     children: React.ReactNode
 }) {
-    return
-    <SessionProvider
-        // Session will be refetched when window focuses
-        refetchOnWindowFocus={true}
-        // Session will be refetched every 5 minutes
-        refetchInterval={5 * 60}>
-        {children}
-    </SessionProvider>
+    return (
+        <SessionProvider
+            // Session will be refetched when window focuses
+            refetchOnWindowFocus={true}
+            // Session will be refetched every 5 minutes
+            refetchInterval={5 * 60}>
+            {children}
+        </SessionProvider>
+    )
 }
